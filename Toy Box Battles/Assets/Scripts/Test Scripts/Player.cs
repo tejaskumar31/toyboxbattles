7bbe6cf8a41b98d45e5f2b0bb12282d01a8e836a
tejaskumar31 <tejaskumar31@gmail.com>
2016-09-18 15:45:18 -0400

    Made changes to the scene look and added controller functionality

--- Toy Box Battles/Assets/Scripts/Test Scripts/Player.cs
+++ Toy Box Battles/Assets/Scripts/Test Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
     public float movementSpeed = 10;
     public float turningSpeed = 60;
 
+    public GameObject prefab;
+    public GameObject projectileParticleEffectPrefab;
+    public Transform projectileSpawn;
+
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
@@ -13,5 +17,17 @@ public class Player : MonoBehaviour
 
         float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
         transform.Translate(0, 0, vertical);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnProjectile();
+        }
+    }
+
+    void SpawnProjectile()
+    {
+        Instantiate(projectileParticleEffectPrefab, projectileSpawn.position, Quaternion.identity);
+        GameObject projectile = Instantiate(prefab, projectileSpawn.position, projectileSpawn.rotation) as GameObject;
+        projectile.GetComponent<Rigidbody>().AddForce(projectileSpawn.forward * 1000);
     }
 }
